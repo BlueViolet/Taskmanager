380a8d6034247e40bbbf836959c9131e8b11708e
BlueViolet <465368912@qq.com>
2018-12-06 15:26:56 +0000

    Repository

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\ProjectsRepository;
+
+class ProjectsController extends Controller
+{
+    protected $repo;
+
+    public function __construct(ProjectsRepository $repo)
+    {
+        $this->middleware('auth');  
+        $this->repo = $repo;
+    }
+
+    public function store(Request $request)
+    {
+        $this->repo->create($request);
+    }
+
+    
+}
---
+++ app/Repositories/ProjectsRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use Image;
+
+class ProjectsRepository
+{
+    public function create($request){
+        $request->user()->projects()->create([
+            'name' => $request->name,
+            'thumbnail' => $this->thumbs($request)
+        ]);
+    }
+
+    public function thumbs($request)
+    {
+        if($request->hasFile('thumbnail')){
+            $thumb = $request->thumbnail;
+            $name = $thumb->hashName();
+            $thumb->storeAs('public/thumbs/original', $name);
+            
+            $path = storage_path('app/public/thumbs/cropped/'.$name);
+            Image::make($thumb)->resize(200, 90)->save($path);
+            return $name;
+        }
+    }
+}
